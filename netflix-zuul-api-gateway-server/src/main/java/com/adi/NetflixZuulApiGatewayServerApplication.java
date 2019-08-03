2b85d26964bbe54f9a4d4e7636cc2456fda56c44
aditya <aditya@192.168.1.3>
2019-08-03 12:33:11 +0530

    Adding Zuul API Gateway Server

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/adi/NetflixZuulApiGatewayServerApplication.java
@@ -0,0 +1,25 @@
+package com.adi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class NetflixZuulApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSample() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+
+}
